dce25403add7974577f80daa680c936498fb3f32
diogo-ms <diogomarquessimoes@gmail.com>
2023-08-16 23:57:13 +0200

    FUCK, i'm so dumb man, i was suppose to do this, not the previous useless commit

--- departements/01-ain/script.sh
+++ departements/01-ain/script.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-files="./test_dir/*"
+files="./test_dir/communes-01-ain-copy.geojson"
 
 for file in $files; 
 do 
     echo "greping... $file" 
     grep -rl "nom" $file | xargs sed -i 's/nom/name/g' 
     echo "correctly formatting .geojson files" 
-    cat $file | jq -c '.features[] |= (.id = .properties.code)' >$file.tmp 
+    cat $file | jq -c '.features[] |= (.id = .properties.code | del(.properties.code))' >$file.tmp 
     echo "moving the temporary file into the original" 
     mv $file.tmp $file 
 done 
